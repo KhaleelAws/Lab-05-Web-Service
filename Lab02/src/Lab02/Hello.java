17cbd97811130103db9f60c88f16e71b0d31c1c2
DePows <rowandiepo@gmail.com>
2025-10-09 16:25:13 -0700

    Added my ID and name

--- Lab02/src/Lab02/Hello.java
+++ Lab02/src/Lab02/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("303732481 - Jared Mawdsley.");
 		System.out.println("304792891 - Ethan Tran.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302825666 - Rowan Diepenbrock");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
