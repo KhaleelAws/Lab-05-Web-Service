5827a017f2fba942c70c27c64669deb7a6c51947
Seoji <Seoji@192.168.1.222>
2025-10-09 20:14:12 -0700

    done

--- Lab 2/src/Hello/Hello.java
+++ Lab 2/src/Hello/Hello.java
@@ -1,3 +1,4 @@
+
 package Hello;
 
 public class Hello {
@@ -5,11 +6,10 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
-		// Add a new line with your information
-		System.out.println("303732481 - Jared Mawdsley.");
-		System.out.println("304792891 - Ethan Tran.");
-		System.out.println("302825666 - Rowan Diepenbrock");
-		System.out.println("302414528 - Luz Martinez ");
+		System.out.println("302414528 - Luz Martinez.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
@@ -19,5 +19,4 @@ public class Hello {
 
 		
 }
-
 }
