4efab5d37264f5c818ecddb5870bd940ab2d74fc
Haeun-L <luzmartinezlopez@csus.edu>
2025-10-09 20:21:35 -0700

    Update Hello.java

--- Lab02/src/Lab02/Hello.java
+++ Lab02/src/Lab02/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("303732481 - Jared Mawdsley.");
 		System.out.println("304792891 - Ethan Tran.");
 		System.out.println("302825666 - Rowan Diepenbrock");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302414528 - Luz Martinez.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
@@ -21,3 +21,4 @@ public class Hello {
 }
 
 }
+
